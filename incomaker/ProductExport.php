6a03f06a1b7c81056499e9320babd7b73ea7375b
Karel Zavadil <zavadil@incomaker.com>
2024-11-12 12:31:31 +0100

    products - removed attributes, refactored

--- incomaker/ProductExport.php
+++ incomaker/ProductExport.php
@@ -42,19 +42,27 @@ class ProductExport extends XmlExport
 		return wc_get_products($this->getQuery());
 	}
 
-	private $thisProduct;
-
 	protected function createXml($product) {
 		$childXml = $this->xml->addChild('p');
 		$childXml->addAttribute("id", $product->get_id() * XmlExport::PRODUCT_ATTRIBUTE);
+		$this->addItem($childXml, 'productId', $product->get_id());
 		$imageUrl = wp_get_attachment_image_url($product->get_image_id(), 'full');
 		if (!empty($imageUrl)) {
 			$this->addItem($childXml, 'imageUrl', $imageUrl);
 		}
+
+		$this->addItem($childXml, 'stock', $product->get_stock_quantity());
+		$this->addItem($childXml, 'choiceFlag', $product->get_featured() ? 1 : 0);
+		$this->addItem($childXml, 'active', ($product->get_status() == "publish" ? 1 : 0));
+		$this->addItem($childXml, 'updated', $product->get_date_modified());
+
+		// CATEGORIES
 		$categoriesXml = $childXml->addChild('categories');
 		foreach ($product->get_category_ids() as $value) {
 			$categoriesXml->addChild('c', $value);
 		}
+
+		// PRICES
 		$pricesXml = $childXml->addChild('prices');
 		$pXml = $pricesXml->addChild('p');
 		$pXml->addAttribute("currency", get_woocommerce_currency());
@@ -67,17 +75,7 @@ class ProductExport extends XmlExport
 			$this->addItem($pXml, "priceAfterDiscount", $sale_price);
 		}
 
-		$tagsXml = $childXml->addChild('tags');
-		foreach ($product->get_tag_ids() as $value) {
-			$tagsXml->addChild('t', get_term($value)->name);
-		}
-		$tagsXml->addChild('t', $this->getLang());
-
-		$this->addItem($childXml, 'stock', $product->get_stock_quantity());
-		$this->addItem($childXml, 'choiceFlag', $product->get_featured() ? 1 : 0);
-		$this->addItem($childXml, 'active', ($product->get_status() == "publish" ? 1 : 0));
-		$this->addItem($childXml, 'updated', $product->get_date_modified());
-
+		// LABELS
 		$languagesXml = $childXml->addChild('languages');
 		$lXml = $languagesXml->addChild('l');
 		$lXml->addAttribute("id", $this->getLang());
@@ -86,20 +84,26 @@ class ProductExport extends XmlExport
 		$this->addItem($lXml, "shortDescription", self::removeXmlInvalidChars($product->get_short_description()));
 		$this->addItem($lXml, 'url', get_permalink($product->get_id()));
 
+		// TAGS
+		$tagsXml = $childXml->addChild('tags');
+		foreach ($product->get_tag_ids() as $value) {
+			$tagsXml->addChild('t', get_term($value)->name);
+		}
+
+		//export language as tag
+		$tagsXml->addChild('t', $this->getLang());
+
+		//export attributes as tags
 		$attributes = $product->get_attributes();
 		if (!empty($attributes)) {
-			$attributesXml = $childXml->addChild('attributes');
 			foreach ($attributes as $attribute) {
 				$attribute_values = $attribute->is_taxonomy() ? wc_get_product_terms($product->get_id(), $attribute->get_name(), array('fields' => 'names')) : $attribute->get_options();
 				foreach ($attribute_values as $value) {
-					$aXml = $attributesXml->addChild('a', $value);
-					$aXml->addAttribute("key", wc_attribute_label($attribute->get_name()));
-					$tagsXml->addChild('t', $value); //use attributes as tags as well
+					//$aXml->addAttribute("key", wc_attribute_label($attribute->get_name()));
+					$tagsXml->addChild('t', $value);
 				}
 			}
 		}
-
-		$this->addItem($childXml, 'productId', $product->get_id());
 
 		do_action('incomaker_modify_xml_product_item', $this, $childXml, $product);
 	}
